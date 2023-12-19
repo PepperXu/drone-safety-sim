63179be717efd6eb46292818ddb7447bfb6e3302
PepperXu <XPS83535135@gmail.com>
2023-12-19 09:21:00 +0800

    monitor-sfx

--- Assets/Scripts/UIUpdater.cs
+++ Assets/Scripts/UIUpdater.cs
@@ -9,17 +9,18 @@ public class UIUpdater : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI flightState, missionState, systemState, controlState;
     [SerializeField] AudioSource audioSource;
-    [SerializeField] AudioClip monitoringClip;
-    const float healthyInterval = 1.2f, warningInterval = 0.8f, emergencyInterval = 0.4f;
-    bool enableSound = true;
+    [SerializeField] AudioClip beep, monitoring_ok, monitoring_warn, monitoring_alert;
+    public float healthyInterval = 1.2f, warningInterval = 0.8f, emergencyInterval = 0.4f;
+    public bool enableSound = false;
+    bool continuous = true;
     float currentMonitoringInterval = 1.2f;
     float monitoringTimer = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        audioSource.clip = monitoringClip;
-        StartCoroutine(PlayMonitoringSound());
+
     }
+
 
     // Update is called once per frame
     void Update()
@@ -29,20 +30,64 @@ public class UIUpdater : MonoBehaviour
         systemState.text = Enum.GetName(typeof(DroneManager.SystemState), DroneManager.currentSystemState);
         controlState.text = Enum.GetName(typeof(DroneManager.ControlType), DroneManager.currentControlType);
 
-        switch (DroneManager.currentSystemState)
+        if (enableSound)
         {
-            case DroneManager.SystemState.Healthy:
-                currentMonitoringInterval = healthyInterval;
-                break;
-            case DroneManager.SystemState.Warning:
-                currentMonitoringInterval = warningInterval;
-                break;
-            case DroneManager.SystemState.Emergency:
-                currentMonitoringInterval = emergencyInterval;
-                break;
-            default:
-                Debug.LogError("System State Undefined");
-                break;
+            if (continuous)
+            {
+                audioSource.loop = true;
+            } else
+            {
+                audioSource.loop = false;
+                if(audioSource.clip != beep)
+                    audioSource.clip = beep;
+            }
+            switch (DroneManager.currentSystemState)
+            {
+                case DroneManager.SystemState.Healthy:
+                    if (continuous)
+                    {
+                        if (audioSource.clip != monitoring_ok)
+                        {
+                            audioSource.clip = monitoring_ok;
+                            audioSource.Play();
+                        }
+
+                    }
+                    else
+                        currentMonitoringInterval = healthyInterval;
+                    break;
+                case DroneManager.SystemState.Warning:
+                    if (continuous)
+                    {
+                        if (audioSource.clip != monitoring_warn)
+                        {
+                            audioSource.clip = monitoring_warn;
+                            audioSource.Play();
+                        }
+                    }
+                    else
+                        currentMonitoringInterval = warningInterval;
+                    break;
+                case DroneManager.SystemState.Emergency:
+                    if (continuous)
+                    {
+                        if (audioSource.clip != monitoring_alert)
+                        {
+                            audioSource.clip = monitoring_alert;
+                            audioSource.Play();
+                        }
+                    }
+                    else
+                        currentMonitoringInterval = emergencyInterval;
+                    break;
+                default:
+                    Debug.LogError("System State Undefined");
+                    break;
+            }
+        } else
+        {
+            if (audioSource.isPlaying)
+                audioSource.Stop();
         }
     }
 
