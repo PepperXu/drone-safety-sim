af56a3908ecd355dda3002ea764ff903f7c9e8a1
PepperXu <XPS83535135@gmail.com>
2024-05-08 13:09:08 +0800

    vehicle sim

--- Assets/TrafficSimulation.cs
+++ Assets/TrafficSimulation.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class TrafficSimulation : MonoBehaviour
 {
+
+    [SerializeField] Transform[] spawnLanes;
+    [SerializeField] GameObject[] vehiclePrefabs;
+    private int currentSpawnLane = 0;
+    private const float spawnInterval = 3f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnVehicles());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator SpawnVehicles()
+    {
+        while (true)
+        {
+            int randomIndex = Random.Range(0, vehiclePrefabs.Length);
+            GameObject go = Instantiate(vehiclePrefabs[randomIndex]);
+            go.transform.position = spawnLanes[currentSpawnLane].position;
+            go.transform.eulerAngles = spawnLanes[currentSpawnLane].eulerAngles;
+            go.transform.localScale *= 1.3f;
+            go.GetComponent<Vehicle>().SetLane(spawnLanes[currentSpawnLane]);
+            currentSpawnLane++;
+            if(currentSpawnLane >= spawnLanes.Length)
+            {
+                currentSpawnLane = 0;
+            }
+            yield return new WaitForSeconds(spawnInterval);
+        }
     }
 }
--- Assets/Vehicle.cs
+++ Assets/Vehicle.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class Vehicle : MonoBehaviour
 {
+    private Transform currentLane;
+    private const float speed = 20f, spinningAngularSpeed = 5000f;
+    private int currentWaypointIndex = 0;
+    private List<Transform> wheels = new List<Transform>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach(Transform t in transform)
+        {
+            if (t.gameObject.name.Contains("Wheel"))
+            {
+                wheels.Add(t);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(currentWaypointIndex < currentLane.childCount)
+        {
+            float distanceBetweenNowAndWp = Vector3.Distance(transform.position, currentLane.GetChild(currentWaypointIndex).position);
+            if (distanceBetweenNowAndWp < 0.2f)
+            {
+                currentWaypointIndex++;
+            } else
+            {
+                float anglesBetweenNowAndWP = Vector3.SignedAngle(transform.forward, currentLane.GetChild(currentWaypointIndex).position - transform.position, Vector3.up);
+                float angularTurnSpeed = anglesBetweenNowAndWP * (speed/5f);
+                transform.eulerAngles += transform.up * angularTurnSpeed * Time.deltaTime;
+                transform.position += transform.forward * speed * Time.deltaTime;
+                
+            }
+        } else
+        {
+            Destroy(gameObject);
+        }
+        foreach(Transform t in wheels)
+        {
+            t.transform.Rotate(Vector3.right, spinningAngularSpeed * Time.deltaTime);
+        }
+    }
+
+    public void SetLane(Transform lane)
+    {
+        currentLane = lane; 
     }
 }
